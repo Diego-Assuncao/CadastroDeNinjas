bb81cd10dd0224868f1bfc4e024501595a23ae1e
Diego Assunção <diegoassuncao.dev@gmail.com>
2026-08-25 21:41:38 -0300

    fix: adicionando @Column na classe NinjaModel

--- src/main/java/dev/diego/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/diego/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,59 +2,37 @@ package dev.diego.CadastroDeNinjas.Ninjas;
 
 import dev.diego.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe comum em uma entidade do DB
 @Entity
 //Criando tabela do DB
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     //Atributos
     @Id//informa que o atributo abaixo será o id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Informa a estratégia de geração do id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
     @Column(unique = true) //Informa que a coluna é única, utilizamos sempre que estivermos tratando de dados unícos como CPF
     private String email;
+
+    @Column(name = "idade")
     private int idade;
 
     @ManyToOne//@ManyToOne um ninja tem uma única missão
     @JoinColumn(name = "missoes_id") //foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    //Constructors
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String email, String nome, int idade) {
-        this.email = email;
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    //Getters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    //Setters
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
